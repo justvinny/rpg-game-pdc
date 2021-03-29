36ff00e00ed8d21dd413c080366bb6e02322f067
justvinny <vinsonemb.151994@gmail.com>
2021-03-29 16:23:45 +1300

    Added utility class to read maps from text files

---
+++ src/main/java/com/mycompany/pdc_assignment_rpg/utilities/MapLoaderUtility.java
@@ -0,0 +1,64 @@
+package com.mycompany.pdc_assignment_rpg.utilities;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utility class to for loading game maps from text files.
+ *
+ * @author Vinson Beduya - 19089783 <vinsonemb.151994@gmail.com>
+ */
+public class MapLoaderUtility {
+
+    /**
+     * Method to load a map from a text file depending on map name argument
+     * given. Once successfully loaded, the map will be stored in a list.
+     *
+     * @param mapName name of the map text file we're going to load.
+     * @return a List collection containing a map for our game.
+     */
+    public static List<String> loadMap(String mapName) {
+        List<String> map = new ArrayList<>();
+
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(mapName));
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+            }
+        } catch (IOException ex) {
+            System.err.println(ex.getMessage());
+        } finally {
+            // Ensures that the reader is closed.
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+        }
+
+        return map;
+    }
+
+    /**
+     * Method to ensure the text file we're loading is a valid map.
+     *
+     * @return a boolean value on whether the map loaded is valid or not.
+     */
+    public static boolean isValidMap(List<String> gameMap) {
+        // Check list is not empty and then get the length of the first index
+        // which will be used to ensure that it is valid map that has the same
+        // length for all the strings.
+        int mapWidth = (gameMap.size() > 0) ? gameMap.get(0).length() : 0;
+
+        // Compares length of each line in the map.
+        return gameMap.stream().noneMatch(line -> (mapWidth != line.length()));
+    }
+
+}
